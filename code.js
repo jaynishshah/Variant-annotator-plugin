d57e2070cf794c48074009465c8fd8a267c142fb
jaynish@commandspace.co <jaynish.shah22@gmail.com>
2022-05-07 23:40:39 +0530

    Updated code

--- code.js
+++ code.js
@@ -1,40 +1,45 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
-// This shows the HTML page in "ui.html".
-//      figma.showUI(__html__);
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-//      figma.ui.onmessage = msg => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    // if (msg.type === 'create-rectangles') {
-    //     const nodes = [];
-    //     for (let i = 0; i < msg.count; i++) {
-    //         const rect = figma.createRectangle();
-    //         rect.x = i * 150;
-    //         rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-    //         figma.currentPage.appendChild(rect);
-    //         nodes.push(rect);
-    //     }
-    //     figma.currentPage.selection = nodes;
-    //     figma.viewport.scrollAndZoomIntoView(nodes);
-    // }
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-   // figma.closePlugin("Rectangles got created!");
-// };
 
+//Defining a component instance
+let selectedInstance = figma.currentPage.selection[0]
+let positionX = (selectedInstance.x);
+let positionY = (selectedInstance.y) - 100;
 
-// Check if the selection is an Instance, and accordingly show messages
+//load font for the text box to render
+async function loadFont() {
 
-if (figma.currentPage.selection[0].type === 'INSTANCE') {
+    await figma.loadFontAsync({
+        family: 'Inter',
+        style: 'Regular',
+    });
+  
+  };
+
+  (async() => {
+    await loadFont()
+
+// Check if the user has selected an object that's not a component instance
+if (figma.currentPage.selection.length !== 1) {
+    figma.closePlugin("Please select component instances to annotate")
+}
+else if (selectedInstance.type !== 'INSTANCE') {
+    figma.closePlugin("Please select component instances to annotate")
+}
+
+// Check if the user has selected an object that's a component instance
+// Create a text box
+else if (selectedInstance.type === 'INSTANCE') {
+    const nodes = [];
+        for (let i = 0; i < 1; i++) {
+            const text = figma.createText();
+             text.x = positionX;
+             text.y = positionY;
+             text.fontSize = 16;
+             text.characters = "this is a component instance";
+            figma.currentPage.appendChild(text);
+            nodes.push(text);
+        }
+        figma.currentPage.selection = nodes;
+    //    figma.viewport.scrollAndZoomIntoView(nodes);
     figma.closePlugin("Annotating Variants")
 }
-
-if (figma.currentPage.selection[0].type !== 'INSTANCE') {
-    figma.closePlugin("Please select instances to annotate")
-}
+})();
