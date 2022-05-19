acf67a26880b19a1d05ef2d2201e0a88b1b28a5b
jaynish@commandspace.co <jaynish.shah22@gmail.com>
2022-05-19 19:42:08 +0530

    Properties rendering as string in JSON format

--- code.js
+++ code.js
@@ -1,27 +1,66 @@
 
-//Defining a component instance
-let selectedInstance = figma.currentPage.selection[0]
-let positionX = (selectedInstance.x);
-let positionY = (selectedInstance.y) - 100;
 
-//load font for the text box to render
-async function loadFont() {
+// //load font for the text box to render
+// async function loadFont() {
 
-    await figma.loadFontAsync({
-        family: 'Inter',
-        style: 'Regular',
-    });
+//     await figma.loadFontAsync({
+//         family: 'Inter',
+//         style: 'Regular',
+//     });
   
-  };
+//   };
 
-  (async() => {
-    await loadFont()
+//   (async() => {
+//     await loadFont()
+
+// // Check if the user has selected an object that's not a component instance
+// if (figma.currentPage.selection.length !== 1) {
+//     figma.closePlugin("Please select component instances to annotate")
+// }
+// else if (selectedInstance.type !== 'INSTANCE') {
+//     figma.closePlugin("Please select component instances to annotate")
+// }
+
+// // Check if the user has selected an object that's a component instance
+// // Create a text box
+// else if (selectedInstance.type === 'INSTANCE') {
+//     const nodes = [];
+//         for (let i = 0; i < 1; i++) {
+//             const text = figma.createText();
+//              text.x = positionX;
+//              text.y = positionY;
+//              text.fontSize = 16;
+//              text.characters = "this is a component instance";
+//             figma.currentPage.appendChild(text);
+//             nodes.push(text);
+//         }
+//         figma.currentPage.selection = nodes;
+//     //    figma.viewport.scrollAndZoomIntoView(nodes);
+//     figma.closePlugin("Annotating Variants")
+// }
+// })();
 
 // Check if the user has selected an object that's not a component instance
 if (figma.currentPage.selection.length !== 1) {
-    figma.closePlugin("Please select component instances to annotate")
+    figma.closePlugin("Nothing selected. Please select component instances to annotate.")
 }
-else if (selectedInstance.type !== 'INSTANCE') {
+
+async function main() {
+
+//Defining a component instance
+let selectedInstance = figma.currentPage.selection[0];
+let positionX = (selectedInstance.x);
+let positionY = (selectedInstance.y) - 100;
+
+const instancePropInfo = [selectedInstance.variantProperties];
+const instancePropText = JSON.stringify(instancePropInfo);
+
+await figma.loadFontAsync({
+            family: 'Inter',
+            style: 'Regular',
+        });
+
+if (selectedInstance.type !== 'INSTANCE') {
     figma.closePlugin("Please select component instances to annotate")
 }
 
@@ -34,7 +73,7 @@ else if (selectedInstance.type === 'INSTANCE') {
              text.x = positionX;
              text.y = positionY;
              text.fontSize = 16;
-             text.characters = "this is a component instance";
+             text.characters = instancePropText;
             figma.currentPage.appendChild(text);
             nodes.push(text);
         }
@@ -42,4 +81,7 @@ else if (selectedInstance.type === 'INSTANCE') {
     //    figma.viewport.scrollAndZoomIntoView(nodes);
     figma.closePlugin("Annotating Variants")
 }
-})();
+
+}
+
+main();
