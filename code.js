0cff17420bcb92cf59238614f712a3007f415a5f
Jaynish Shah <jaynish.shah22@gmail.com>
2025-09-07 17:56:00 +0100

    Notify when instance has no annotatable properties

--- code.js
+++ code.js
@@ -38,6 +38,7 @@ function main() {
             const hasVariantProps = Object.keys(variantProps).length > 0;
             const hasComponentProps = Object.keys(componentProps).length > 0;
             if (!hasVariantProps && !hasComponentProps) {
+                figma.notify(`Instance "${item.name}" has no annotatable properties.`);
                 continue;
             }
             const bounds = item.absoluteRenderBounds;
